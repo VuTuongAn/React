a2abf312598545c98e1912d9d205560cdf7b9ea7
vutuongan <vutuongan2003@gmail.com>
2024-06-14 10:34:05 +0700

    useState

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const name = 'Anvu';
   const nameObj = {
     name: 'Anvu',
     age: 25,
     email: 'vuan@gmail.com'
   }
+  const [name, setName] = React.useState('Anvu');
+  const handleChangeName = () => {
+    setName('Name anvu new');
+  }
   return (
     <div className="App">
-      <div>{name}</div>
       <div>{nameObj.name}</div>
       <div>{nameObj.age}</div>
       <div>{nameObj.email}</div>
+      <div>{name}</div>
+      <button onClick={handleChangeName}>Change Name</button>
     </div>
   );
 }
