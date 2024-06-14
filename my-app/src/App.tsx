7dd1383e3b6efa8db26cf57355f6d0e8e821f624
vutuongan <vutuongan2003@gmail.com>
2024-06-14 10:26:20 +0700

    Dynamic values

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Anvu';
+  const nameObj = {
+    name: 'Anvu',
+    age: 25,
+    email: 'vuan@gmail.com'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{name}</div>
+      <div>{nameObj.name}</div>
+      <div>{nameObj.age}</div>
+      <div>{nameObj.email}</div>
     </div>
   );
 }
