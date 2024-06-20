b198f2fe254a4754a222ba1561db4ccf14cb170e
vutuongan <vutuongan2003@gmail.com>
2024-06-20 09:21:40 +0700

    useRef

--- my-app/src/pages/HomPage.tsx
+++ my-app/src/pages/HomPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import CardPizza from "../sections/CardPizza";
 import Pizza from "../models/pizza.model";
 import ButtonField from "../components/ButtonFiled";
@@ -7,12 +7,17 @@ import TextFiled from "../components/TextFiled";
 
 const HomePage = () => {
     const [pizzas, setPizza] = useState<Pizza[]>([])
-
+    const [isLoading] = useState(false);
+    const pageRef = useRef<number>(1);
+    const pageRefChangeHTML = useRef<HTMLDivElement>(null);
+    console.log('PageRef', pageRef);
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(res => res.json())
             .then(data => setPizza(data));
     }, []);
+    ;
+
 
     return (
         <div style={{ height: 'calc(100vh - 309px)', padding: '4rem 4rem', overflowY: 'auto' }}>
@@ -24,8 +29,11 @@ const HomePage = () => {
                     ))
                 }
             </div>
+            <button onClick={() => console.log('PageRef', pageRef.current)}>Show value page</button>
+            <div ref={pageRefChangeHTML}>Vu Tuong An</div>
             <div style={{ display: "flex", justifyContent: "center", width: '100%' }}>
-                <ButtonField>Show more</ButtonField>
+                <ButtonField loading={isLoading} onClick={() => pageRefChangeHTML.current?.setAttribute('style', 'color : red')}>Show more</ButtonField>
+                <ButtonField loading={isLoading} onClick={() => pageRef.current = pageRef.current + 1}>Show more</ButtonField>
             </div>
         </div>
     );
